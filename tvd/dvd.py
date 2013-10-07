10ad5dfe9e335c4aca1fe6633e5d03e508c6ca1c
Hervé BREDIN <bredin@limsi.fr>
2013-10-07 10:17:33 +0200

    feat(TVSeriesDVD): added all-in-one .dump() method

--- tvd/dvd.py
+++ tvd/dvd.py
@@ -25,12 +25,9 @@
 # SOFTWARE.
 #
 
-"""Utilities for dealing with TV series DVDs"""
 
 import os
-import sys
 import subprocess
-import numpy as np
 from lxml import objectify
 
 
@@ -58,8 +55,7 @@ class DVD(object):
             lsdvd = 'lsdvd'
 
         # by default `lsdvd` will look for physical DVD
-        if dvd is None:
-            dvd = ''
+        self.dvd = '' if dvd is None else dvd
 
         if xml is None:
 
@@ -67,7 +63,7 @@ class DVD(object):
             # TODO: error handle
             with open(os.devnull, mode='w') as f:
                 xml_content = subprocess.check_output(
-                    [lsdvd, '-Ox', '-avs', dvd], stderr=f
+                    [lsdvd, '-Ox', '-avs', self.dvd], stderr=f
                 )
 
         elif isinstance(xml, file):
@@ -154,137 +150,3 @@ class DVDSubtitle(object):
         self.langcode = str(subp.langcode)
 
 
-class TVSeriesDVD(DVD):
-    """
-
-    Parameters
-    ----------
-    name : str
-        Name of the TV series (e.g. "The Big Bang Theory")
-    season : int
-        Season number (e.g. 1 for first season)
-    first : int
-        Episode number ()
-    dvd : str, optional
-        Path to dumped DVD directory (with "vobcopy -m -o `dvd`").
-        Default uses physical DVD.
-    lsdvd : str, optional
-        Path to `lsdvd` command line tool (default to "lsdvd")
-    xml : str or file, optional
-        Use XML file precomputed by `lsdvd -Ox -avs`.
-        When provided, both `dvd` and `lsdvd` parameters have no effect.
-    """
-    def __init__(self, name, season, first, dvd=None, lsdvd=None, xml=None):
-        super(TVSeriesDVD, self).__init__(dvd=dvd, lsdvd=lsdvd, xml=xml)
-        self.name = name
-        self.season = season
-        self.first = first
-        self.tolerance = 0.2
-
-    def guess_episode_duration(self, number=None):
-        """Try and guess the average duration of an episode
-
-        This heuristic relies on the following assumptions:
-        - episodes are usually among the longest tracks of the DVD
-        - there are at least 3 episodes in every DVD
-
-        Parameters
-        ----------
-        number : int, optional
-            Number of episodes in DVD.
-
-        Returns
-        -------
-        duration : float
-            Average episode duration
-
-        """
-
-        # if number is not known in advance
-        # use 3 by default
-        if number is None:
-            number = 3
-
-        # sort titles by duration (longest first)
-        titles, durations = zip(
-            *sorted(
-                [(t, t.duration) for t in self.titles],
-                key=lambda (title, duration): duration,
-                reverse=True,
-            )
-        )
-
-        # estimate episode duration as
-        # median of n+1 longest titles
-        return np.median(durations[:number+1])
-
-    def find_episodes(self, duration=None, number=None):
-        """
-
-        This heuristic relies on the following assumptions:
-        - episodes are usually among the longest tracks of the DVD
-        - there are at least 3 episodes in every DVD
-        - the very first track may contain the concatenation of all episodes
-        - episodes are stored in chronological order on the DVD
-
-        Parameters
-        ----------
-        duration : float, optional
-            Average episode duration.
-            Try to guess it automatically when not provided.
-        number : int, optional
-            Number of episodes in DVD.
-            Try to guess it automatically when not provided.
-
-        Returns
-        -------
-        episodes : list of `DVDTitle`
-            List of titles found to contain each episodes.
-        """
-
-        tolerance = 0.2
-
-        if duration is None:
-            target_duration = self.guess_episode_duration(number=number)
-        else:
-            target_duration = duration
-
-        # duration of first track
-        firstDuration = self.titles[0].duration
-
-        # first track is very likely to contain all episodes
-        # if its duration is more than two times the episode duration
-        if firstDuration > 2 * target_duration:
-            firstTitleContainsAll = True
-        else:
-            firstTitleContainsAll = False
-
-        # detected episodes so far and their total duration
-        episodes = []
-        sum_duration = 0.
-
-        for title in self.titles:
-
-            # title duration
-            duration = title.duration
-
-            # skip title if its duration is not close enough to target duration
-            if np.abs(np.log(duration/target_duration)) > np.log(1+tolerance):
-                continue
-
-            if number:
-                # stop looking for episodes if we found enough already
-                if len(episodes) > number:
-                    break
-
-            # skip track if adding it would not match first title duration
-            if firstTitleContainsAll:
-                if sum_duration + duration > firstDuration + 0.5 * target_duration:
-                    break
-
-            # if we passed all previous test,
-            # then it is an episode
-            episodes.append(title)
-            sum_duration = sum_duration + title.duration
-
-        return episodes
