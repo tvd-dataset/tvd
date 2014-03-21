55282443b5362d455759cb4aedf9bea00cd1571e
Hervé BREDIN <bredin@limsi.fr>
2014-03-21 14:51:17 +0100

    chore: PathMixin methods assume that instance has attribute 'tvd_dir'

--- tvd/series/path.py
+++ tvd/series/path.py
@@ -28,82 +28,71 @@
 
 class PathMixin(object):
 
-    def path_to_dump(self, disc, tvd_dir=None):
+    def path_to_dump(self, season, disc):
 
         pattern = (
             '{tvd}/{series}/dvd/dump/'
             'Season{season:02d}.Disc{disc:02d}'
         )
 
-        if tvd_dir is None:
-            raise ValueError('')
-
         return pattern.format(
-            tvd=tvd_dir,
+            tvd=self.tvd_dir,
             series=self.__class__.__name__,
+            season=season,
             disc=disc
         )
 
-    def path_to_video(self, episode, tvd_dir=None):
+    def path_to_video(self, episode):
 
         pattern = (
             '{tvd}/{series}/dvd/rip/video/'
             '{series}.Season{season:02d}.Episode{episode:02d}.mkv'
         )
 
-        if tvd_dir is None:
-            raise ValueError('')
-
         return pattern.format(
-            tvd=tvd_dir,
+            tvd=self.tvd_dir,
             series=episode.series,
             season=episode.season,
             episode=episode.episode,
         )
 
-    def path_to_audio(self, episode, language=None, tvd_dir=None):
+    def path_to_audio(self, episode, language=None):
 
         pattern = (
             '{tvd}/{series}/dvd/rip/audio/'
             '{series}.Season{season:02d}.Episode{episode:02d}.{language}.wav'
         )
 
-        if tvd_dir is None:
-            raise ValueError('')
-
         if language is None:
             language = self.language
 
         return pattern.format(
-            tvd=tvd_dir,
+            tvd=self.tvd_dir,
             series=episode.series,
             season=episode.season,
             episode=episode.episode,
             language=language
         )
 
-    def path_to_subtitles(self, episode, language=None, tvd_dir=None):
+    def path_to_subtitles(self, episode, language=None):
 
         pattern = (
             '{tvd}/{series}/dvd/rip/subtitles/'
             '{series}.Season{season:02d}.Episode{episode:02d}.{language}.srt'
         )
 
-        if tvd_dir is None:
-            raise ValueError('')
-
         if language is None:
             language = self.language
 
         return pattern.format(
-            tvd=tvd_dir,
+            tvd=self.tvd_dir,
             series=episode.series,
             season=episode.season,
             episode=episode.episode,
             language=language
         )
 
-    def path_to_stream(self, episode, format='mp4', tvd_dir=None):
+    def path_to_stream(self, episode, format='mp4'):
         """
         Parameters
         ----------
@@ -112,20 +101,31 @@ class PathMixin(object):
         """
 
         assert format in ['mp4', 'ogv', 'webm']
-        assert tvd_dir is not None
 
         pattern = (
             '{tvd}/{series}/dvd/rip/stream/'
             '{series}.Season{season:02d}.Episode{episode:02d}.{format}'
         )
 
-        if tvd_dir is None:
-            raise ValueError('')
-
         return pattern.format(
-            tvd=tvd_dir,
+            tvd=self.tvd_dir,
             series=episode.series,
             season=episode.season,
             episode=episode.episode,
             format=format
+        )
+
+    def path_to_resource(self, episode, resource):
+        
+        pattern = (
+            '{tvd}/{series}/www/{resource}/'
+            '{series}.Season{season:02d}.Episode{episode:02d}.json'
+        )
+
+        return pattern.format(
+            tvd=self.tvd_dir,
+            series=episode.series,
+            resource=resource,
+            season=episode.season,
+            episode=episode.episode
         )
