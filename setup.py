17f413bb372e7716e01bac6d3c6b6f5539e88e6d
Hervé BREDIN <bredin@limsi.fr>
2015-10-27 17:22:32 +0100

    doc: CHANGELOG and setup.py

--- setup.py
+++ setup.py
@@ -71,7 +71,11 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Scientific/Engineering"
     ]
 )
